665e0049853df11ba1288b448c162f286521442c
Rohitvarma-ujeeni <Rohitvarma.ujeeni@gmail.com>
2025-07-03 21:34:13 +0530

    Update AccountMicroserviceApplication.java

--- src/main/java/com/ABCD/account/AccountMicroserviceApplication.java
+++ src/main/java/com/ABCD/account/AccountMicroserviceApplication.java
@@ -1,3 +1,11 @@
 package com.ABCD.account;
-...
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountMicroserviceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AccountMicroserviceApplication.class, args);
+    }
 }
